ba2f0c1a489444a601f5182fa4e37874e6921dad
DaniilKubantsev <daniil.kubantsev@gmail.com>
2024-06-05 13:40:15 +0300

    front

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,10 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
-//= link_tree ../../javascript .js
-//= link_tree ../../../vendor/javascript .js
+//= link main.css
+//= link application.css
+//= link pagy.js
+//= link pagy.css
+//= link application.js
+//= link controllers/application.js
+//= link controllers/hello_controller.js
+//= link controllers/index.js
